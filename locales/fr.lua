803fdfc0646e09b829bae23171ad13ee56598e2b
RubnK <40402492+RubnK@users.noreply.github.com>
2020-04-19 15:27:48 +0200

    Update locale fr.lua
    It is better to have a good translation.

--- locales/fr.lua
+++ locales/fr.lua
@@ -1,8 +1,8 @@
 Locales['fr'] = {
-  ['que_haces'] = 'Que faites-vous ici, si vous nêtes pas un bûcheron?',
+  ['que_haces'] = 'Que faites-vous ici, si vous n\'êtes pas un bûcheron?',
   ['dame_madera'] = 'Appuyez sur ~INPUT_CONTEXT~ pour vendre du bois',
-  ['no_soy_lenador'] = 'Vous nêtes pas un bûcheron, revenez quand vous êtes',
-  ['no_eres'] = 'Vous nêtes pas un bûcheron',
+  ['no_soy_lenador'] = 'Vous n\'êtes pas un bûcheron, revenez quand vous le serez',
+  ['no_eres'] = 'Vous n\'êtes pas un bûcheron',
   ['te_has_alejado'] = 'Vous avez quitté la zone de travail, la hache a été retirée',
-  ['coger_herramienta'] = 'Appuyez sur ~INPUT_CONTEXT~ pour obtenir Ioutil de travail',
+  ['coger_herramienta'] = 'Appuyez sur ~INPUT_CONTEXT~ pour obtenir Ies outils de travail',
 }
