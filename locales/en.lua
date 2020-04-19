20b6264eecab2c4e17d5b73277334409c6a16cc7
RubnK <40402492+RubnK@users.noreply.github.com>
2020-04-19 15:29:30 +0200

    Update locale en.lua
    I added a "e" to axe

--- locales/en.lua
+++ locales/en.lua
@@ -3,6 +3,6 @@ Locales['en'] = {
   ['dame_madera'] = 'Press ~INPUT_CONTEXT~ to sell wood',
   ['no_soy_lenador'] = 'You are not a lumberjack, come back when you are',
   ['no_eres'] = 'You are not a lumberjack',
-  ['te_has_alejado'] = 'You have moved away from the work area, the ax has been removed',
+  ['te_has_alejado'] = 'You have moved away from the work area, the axe has been removed',
   ['coger_herramienta'] = 'Press ~INPUT_CONTEXT~ to get the work tool',
 }
